504e744410cd3eee061952f90c3ecd09f6c70861
keven01 <kevenchristiancode@gmail.com>
2026-07-04 16:46:07 -0300

    feat: tipos de variaveis

---
+++ variaveis/src/Variaveis.java
@@ -0,0 +1,18 @@
+public class Variaveis {
+    static void main(String[] args) {
+        byte n = -128;
+        short m = -32768;
+        int a = 1;
+        long p = (long) 6.23;
+
+        double b = 2.0;
+        float d = -1.445f;
+
+        char c = (char) 'A';
+        String nome = "Keven";
+
+        boolean t = true;
+
+
+    }
+}
