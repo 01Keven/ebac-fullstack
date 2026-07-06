176defd26a8c0f1c0dda94f513276cadf01e861b
keven01 <kevenchristiancode@gmail.com>
2026-07-06 18:28:53 -0300

    feat: exercicio modulo 05

--- Estudos/entrada-de-dados/src/Entrada.java
+++ Estudos/entrada-de-dados/src/Entrada.java
@@ -1,11 +1,32 @@
 import java.util.Scanner;
 
 public class Entrada {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Qual nome? ");
+
+        System.out.println("Boas Vindas!");
+        System.out.println("Nome Completo: ");
         String nome = scanner.nextLine();
 
-        System.out.println(nome);
+        System.out.println("CPF: ");
+        String cpf = scanner.nextLine();
+
+        System.out.println("Peso: ");
+        double peso = scanner.nextDouble();
+
+        System.out.println("Altura: ");
+        double altura = scanner.nextDouble();
+
+        scanner.nextLine();
+
+        System.out.println("Estado Civil: ");
+        String estadoCivil = scanner.nextLine();
+
+        System.out.println("\n--- Dados Digitados ---");
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Peso: " + peso);
+        System.out.println("Altura: " + altura);
+        System.out.println("Estado Civil: " + estadoCivil);
     }
 }
