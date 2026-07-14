5bacfd403922bcf038648104df13ff248de2538c
keven01 <kevenchristiancode@gmail.com>
2026-07-14 17:09:27 -0300

    feat: contando vogais em uma palavra

--- Exercicicios/exec10/src/QuantidadeVogais.java
+++ Exercicicios/exec10/src/QuantidadeVogais.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
+
 public class QuantidadeVogais {
+    static int contadorVogais = 0;
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -9,19 +11,48 @@ public class QuantidadeVogais {
         int[] vetorIntereiros = new int[tamanhoVetor];
 
         for (int i = 0; i < tamanhoVetor; i++) {
-            System.out.println("posição:");
+            System.out.println("qual numero: ");
             vetorIntereiros[i] = scanner.nextInt();
         }
 
         ordenarVetorInt(vetorIntereiros);
-
-
+        scanner.nextLine();
+        System.out.println("------------------------------");
+        System.out.println("\nDigite uma palavra");
+        String palavra = scanner.nextLine();
+        quantidadeDeVogais(palavra);
     }
 
     private static void exibirVetor(int[] vetor) {
         for (int i = 0; i < vetor.length; i++) {
-            System.out.println(vetor[i]);
+            System.out.println("numero: " + vetor[i]);
         }
+    }
+
+    private static void quantidadeDeVogais(String palavra) {
+        palavra = palavra.toLowerCase();
+
+        for (int i = 0; i < palavra.length(); i++) {
+
+            char primeiraLetra =  palavra.charAt(i);
+            if (primeiraLetra == 'a') {
+                contadorVogais+= 1;
+            } else if (primeiraLetra == 'e') {
+                contadorVogais+= 1;
+;
+            } else if (primeiraLetra == 'i') {
+                contadorVogais+= 1;
+;
+            } else if (primeiraLetra == 'o') {
+                contadorVogais+= 1;
+;
+            } else if (primeiraLetra == 'u') {
+                contadorVogais+= 1;
+;
+            }
+        }
+            System.out.println("Vogais igual a: " + contadorVogais);
+
     }
 
     private static void ordenarVetorInt(int[] vetor) {
